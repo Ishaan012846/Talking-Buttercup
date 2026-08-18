a24d76990c09b50a991284b0b53d934f02cdb6e8
Ishaan012846 <guptaishaa361@gmail.com>
2026-08-18 18:13:40 +0530

    Set base to /Talking-Buttercup/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // Ensures relative path assets for GitHub Pages!
+  base: '/Talking-Buttercup/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
